4d2fb2ac7afb501f61cd5191811fff0416ece794
Fredrikplusa <Fredrikplusa@Fredrikas-MacBook-Pro.local>
2015-03-02 14:09:25 +0100

    Form på indexsidan
    Lagt till form så man kan posta tweets från indexsidan.

--- index.php
+++ index.php
@@ -8,4 +8,20 @@ offlineUser();
 <meta charset="utf-8">
 <p><a href="logout.php">Logga ut</a></p> <p><a href="profile.php">Mina Sidor</a></p></html>
 
+<div class="row">
+	<div class="col-6">
+		<div id="container"  class="container"><br>
+			<h4 class="">Skriv en tweet!</h4>
+		
+			<form action="<? $user["firstName"]?>" method="POST">
+				<textarea maxlength="200" name="TextArea"><?php if(isset($_POST['TextArea'])) {echo $_POST['TextArea'];}?></textarea>
+
+				<input type="submit"  value="SKICKA">
+
+			</form> 
+		</div>
+	</div>
+	<?php newTweet(); ?> 
+	
+
 <? ShowAllTweets();?>
